0dbd2df5e9cb9813ca1b6a620ee0a5ab20fef286
Dagim Tesfaye <valt.tesfaye@gmail.com>
2023-03-14 18:44:57 +0300

    python with django framework

---
+++ hello/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render (request, "hello/index.html")
+
+def brain(request):
+    return HttpResponse("Hello, Brain!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
